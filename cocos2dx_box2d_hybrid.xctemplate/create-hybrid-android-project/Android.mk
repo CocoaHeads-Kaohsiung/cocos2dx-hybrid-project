81ef5579a541fc0f506ef12872c27315e5455b78
jiaming <jiamingwu15@gmail.com>
2012-12-06 14:35:36 +0800

    fix auto include src in Android.mk

--- cocos2dx_box2d_hybrid.xctemplate/create-hybrid-android-project/Android.mk
+++ cocos2dx_box2d_hybrid.xctemplate/create-hybrid-android-project/Android.mk
@@ -2,30 +2,32 @@ COCOS2DX_ROOT_PATH := __cocos2dxroot__
 
 LOCAL_PATH := $(call my-dir)
 
+$(info *** LOCAL_PATH: $(LOCAL_PATH))
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := game_shared
 
 LOCAL_MODULE_FILENAME := libgame
 
-CLASS_PATH := $(LOCAL_PATH)/../../Classes
+#LOCAL_SRC_FILES := hellocpp/main.cpp \
+#                   ../../Classes/AppDelegate.cpp \
+#                   ../../Classes/HelloWorldScene.cpp \
+#                   ../../Classes/GLES-Render.cpp
+SRC_FILE_LIST := $(addprefix ../,$(wildcard ../Classes/*.cpp))
+LOCAL_SRC_FILES := hellocpp/main.cpp \
+                   $(SRC_FILE_LIST)
+              
+$(info *** LOCAL_SRC_FILES: $(LOCAL_SRC_FILES))
+                   
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes \
+					$(COCOS2DX_ROOT_PATH)/external/Box2D
 
-# add all source files automatically
-dirs := $(shell find $(CLASS_PATH) -type d)
-cppfilestemp1 := $(shell find $(CLASS_PATH) -type d)
-cppfilestemp2 := $(shell find $(cppfilestemp1) -name *.cpp)
-cppfilestemp3 := $(sort $(cppfilestemp2))
-cppfiles := $(subst $(LOCAL_PATH)/,,$(cppfilestemp3))
-LOCAL_SRC_FILES += $(cppfiles)
-LOCAL_C_INCLUDES := $(cppfilestemp1)
-
-
-LOCAL_C_INCLUDES += $(COCOS2DX_ROOT_PATH)/external/Box2D
-                            
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static cocosdenshion_static cocos_extension_static box2d_static
             
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,CocosDenshion/android) \
 $(call import-module,cocos2dx) \
-$(call import-module,extensions) $(call import-module,external/Box2D)
+$(call import-module,extensions) \
+$(call import-module,external/Box2D)
